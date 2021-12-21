18d2984d4da88a92ba7985a64fb5c6069aef0f62
romain.le-miere <romain.le-miere@epita.fr>
2021-12-21 22:36:35 +0100

    sets up the ConnectionManager

---
+++ Assets/Scripts/ConnectionManagerScript.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class ConnectionManagerScript : MonoBehaviour
+{
+    private void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(10, 10, 300, 300));
+        if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
+        {
+            StartButtons();
+        }
+        
+        GUILayout.EndArea();
+    }
+
+    void StartButtons()
+    {
+        if (GUILayout.Button("Host"))
+            NetworkManager.Singleton.StartHost();
+        if (GUILayout.Button("Server"))
+            NetworkManager.Singleton.StartServer();
+        if (GUILayout.Button("Client"))
+            NetworkManager.Singleton.StartClient();
+        ;
+    }
+    
+}
