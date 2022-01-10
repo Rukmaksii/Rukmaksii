102f80961559ed564330404a4dda5f2cd942b93c
m1dugh <romain103paris@gmail.com>
2022-01-10 11:46:43 +0100

    adds GameState enum

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -2,8 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+enum GameState
+{
+    Menu,
+    Playing,
+    Ended
+}
+
 public class GameManagerScript : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
