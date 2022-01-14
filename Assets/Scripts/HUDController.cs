683249c649ca1a99369788536f98bb20d2ae7c24
Emmanuel <emmanuel.vouillon@gmail.com>
2022-01-14 11:28:12 +0100

    adds actions on healthCounter

--- Assets/Scripts/HUDController.cs
+++ Assets/Scripts/HUDController.cs
@@ -8,14 +8,19 @@ public class HUDController : MonoBehaviour
 
     public Slider slider;
 
+    public Text healthCounter;
+
     public void SetMaxHealth(int health)
     {
         slider.maxValue = health;
+        
         slider.value = health;
+        healthCounter.text = "" + health;
     }
 
     public void SetHealth(int health)
     {
         slider.value = health;
+        healthCounter.text = "" + health;
     }
 }
