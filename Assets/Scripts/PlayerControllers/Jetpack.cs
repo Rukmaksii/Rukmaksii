61dcb5cbc031a643a3b817ccd97b8cbb13748db1
m1dugh <romain103paris@gmail.com>
2022-01-16 16:41:03 +0100

    moves scripts into subfolders

---
+++ Assets/Scripts/PlayerControllers/Jetpack.cs
@@ -0,0 +1,148 @@
+﻿using UnityEngine;
+
+namespace PlayerControllers
+{
+    public class Jetpack : MonoBehaviour
+    {
+        /**
+    * <value>the multiplier for the fuel reload</value>
+    */
+        [SerializeField] private float reloadMultiplier = 2.5f;
+
+        public PlayerController Player { get; set; }
+
+        /**
+         * <value>the max duration of the flight</value>
+         */
+        public float FuelDuration;
+
+        /**
+         * <value>the remaining time of use.</value>
+         */
+        private float currentFuelUse;
+
+        public bool IsFlying
+        {
+            get => !this.Player.RigidBody.useGravity;
+            set
+            {
+                if (value == true)
+                {
+                    if (this.CanTakeOff)
+                    {
+                        this.Player.RigidBody.useGravity = false;
+                    }
+                }
+                else
+                {
+                    this.Player.RigidBody.useGravity = true;
+                }
+            }
+        }
+
+
+        /**
+         * <value>the direction in world space to go to (magnitude should be under 1)</value>
+         */
+        private Vector3 _direction = Vector3.zero;
+
+        public Vector3 Direction
+        {
+            get => _direction;
+            set { _direction = Vector3.ClampMagnitude(value, 1f); }
+        }
+
+        /**
+         * <value>the force that will be applied to the Player by the jetpack</value>
+         */
+        [SerializeField] private float jetpackForce = 5f;
+
+        /**
+         * <value>the minimum amount of fuel required to start the engines</value>
+         */
+        [SerializeField] private float minRequiredFuel = 0.25f;
+
+        /**
+     * <value>the max height the player can reach flying</value>
+     */
+        [SerializeField] private float maxHeight = 70f;
+
+        /**
+     * <value>the speed multiplier when the <see cref="Jetpack.IsSwift"/> flag is set</value>
+     */
+        [SerializeField] private float enhancedSpeedMultiplier = 2f;
+
+        /**
+     * <value>a flag allowing the player to go quicker (consuming more fuel) </value>
+     */
+        public bool IsSwift { get; set; }
+
+        /**
+     * <value>a float between 0 and 1 the percentage of fuel used</value>
+     */
+        public float FuelConsumption => currentFuelUse / FuelDuration;
+
+        private bool isReady = false;
+
+        public bool CanTakeOff => FuelConsumption >= minRequiredFuel;
+
+
+        public void FixedUpdate()
+        {
+            HandleFuel();
+
+            if (!isReady)
+            {
+                isReady = CanTakeOff;
+            }
+
+            if (IsFlying && isReady)
+            {
+                var velocity = Direction * jetpackForce;
+                if (this.Player.transform.position.y >= maxHeight)
+                {
+                    velocity.y = 0;
+                }
+
+                if (IsSwift)
+                {
+                    velocity *= enhancedSpeedMultiplier;
+                }
+
+                // TODO : smooth out the velocity changes
+                this.Player.RigidBody.velocity = velocity;
+            }
+        }
+
+
+        private void HandleFuel()
+        {
+            float reloadTime = reloadMultiplier * Time.fixedDeltaTime;
+            if (!this.IsFlying)
+            {
+                if (currentFuelUse + reloadTime > FuelDuration)
+                {
+                    currentFuelUse = FuelDuration;
+                }
+                else
+                {
+                    currentFuelUse += reloadTime;
+                }
+            }
+            else
+            {
+                float fuelConsumption = Time.fixedDeltaTime * (IsSwift ? enhancedSpeedMultiplier : 1f);
+                if (currentFuelUse - fuelConsumption < 0)
+                {
+                    currentFuelUse = 0;
+                    this.IsFlying = false;
+                    this.isReady = false;
+                }
+                else
+                {
+                    currentFuelUse -= fuelConsumption;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Weapons/BaseWeapon.cs
@@ -0,0 +1,167 @@
+using System.Collections;
+using System.Collections.Generic;
+using PlayerControllers;
+using model;
+using UnityEngine;
+
+
+namespace Weapons
+{
+    public class BaseWeapon : MonoBehaviour, IWeapon
+    {
+        public PlayerController Player { get; set; }
+
+        [SerializeField] protected float range;
+
+        public float Range => range;
+
+        [SerializeField] protected int damage;
+        public int Damage => damage;
+
+        /**
+     * <value>the time between each bullet row</value>
+     */
+        [SerializeField] protected float cooldown;
+
+        public float Cooldown => cooldown;
+
+        /**
+     * <value>the remaining time of the <see cref="cooldown"/></value>
+     */
+        protected float remainingCD = 0f;
+
+        [SerializeField] protected int maxAmmo;
+        public int MaxAmmo => maxAmmo;
+
+        protected int currentAmmo;
+        public int CurrentAmmo => currentAmmo;
+
+        [SerializeField] protected float reloadTime;
+        public float ReloadTime => reloadTime;
+
+        protected float remainingReloadTime;
+
+        /**
+     * <value>the current percentage of the reload </value>
+     */
+        public float ReloadRate => remainingReloadTime / reloadTime;
+
+        /**
+     * <value>the number of bullet fired each time the fire is called </value>
+     */
+        [SerializeField] protected int sentBulletsInRow;
+
+        /**
+     * <value>the time in seconds between each bullet in a bullet row (when <see cref="sentBulletsInRow"/> is bigger than 1)</value>
+     */
+        [SerializeField] protected float bulletsInRowSpacing;
+
+        /**
+     * <value>the number of remaining bullets to send in a bullet row</value>
+     */
+        protected int remainingBulletsInRow;
+
+        /**
+     * <value>the current remaining time between the previous bullet and the next bullet in the bullet row</value>
+     */
+        protected float betweenBulletsCurrentCD;
+
+        protected bool isReloading = false;
+
+        /**
+     * <summary>a flag used for multi bullets in a row, true if the player triggered fire and released before the end of the bullet row</summary>
+     */
+        private bool hasBeenFiring = false;
+
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            if (currentAmmo == 0)
+            {
+                Reload();
+            }
+
+            if (!isReloading)
+            {
+                if (sentBulletsInRow > 1)
+                {
+                    // handleMultiBulletFire();
+                }
+                else
+                {
+                    handleSingleBulletFire();
+                }
+            }
+            else
+            {
+                if (remainingReloadTime <= 0)
+                {
+                    isReloading = false;
+                    remainingReloadTime = 0;
+                    currentAmmo = maxAmmo;
+                }
+                else
+                {
+                    remainingReloadTime -= Time.fixedDeltaTime;
+                }
+            }
+        }
+
+
+        /**
+     * <summary>handles the cooldown process between fire when <see cref="sentBulletsInRow"/> is 1</summary>
+     */
+        void handleSingleBulletFire()
+        {
+            if (remainingCD > 0)
+            {
+                remainingCD -= Time.fixedDeltaTime;
+                return;
+            }
+
+            if (this.Player.IsShooting)
+            {
+                Shoot();
+                remainingCD = cooldown;
+            }
+        }
+
+        public void Reload()
+        {
+            this.isReloading = true;
+        }
+
+        /**
+     * <summary>shoots at from the middle of the screen to where looked at</summary>
+     * <remarks>shoots 1 bullet</remarks>
+     * <returns>true if an damageable object has been shot</returns>
+     */
+        private bool Shoot()
+        {
+            GameObject hit;
+
+            if ((hit = this.Player.GetObjectInSight(this.range)) == null)
+                return false;
+
+            if (hit.CompareTag("Player"))
+            {
+                PlayerController enemyPlayer = hit.GetComponent<PlayerController>();
+                if (enemyPlayer == null)
+                {
+                    return false;
+                }
+
+
+                enemyPlayer.TakeDamage(this.damage);
+            }
+            else
+            {
+                // none of the tags has been found
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
