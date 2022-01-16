898cdc5d17114206a3c1b6878d97e01513294472
m1dugh <romain103paris@gmail.com>
2022-01-16 16:56:21 +0100

    adds Test gun

---
+++ Assets/Scripts/Weapons/TestGun.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Weapons
+{
+    public class TestGun : BaseWeapon
+    {
+        public override float Range { get; } = 50f;
+        public override int Damage { get; } = 5;
+        public override float Cooldown { get; } = 1f;
+        public override int MaxAmmo { get; } = 20;
+        public override float ReloadTime { get; } = 3f;
+        public override int BulletsInRow { get; } = 1;
+        public override float BulletsInRowSpacing { get; } = 0;
+    }
+}
