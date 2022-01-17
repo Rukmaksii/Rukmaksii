7e447558dc6e89b5866f62caa1c9d4504a648215
m1dugh <romain103paris@gmail.com>
2022-01-17 18:57:35 +0100

    adds approval check for connection handling

--- Assets/Scripts/GameManagers/ConnectionManagerScript.cs
+++ Assets/Scripts/GameManagers/ConnectionManagerScript.cs
@@ -19,11 +19,34 @@ namespace GameManagers
         void StartButtons()
         {
             if (GUILayout.Button("Host"))
+            {
+                NetworkManager.Singleton.ConnectionApprovalCallback += ApprovalCheck;
                 NetworkManager.Singleton.StartHost();
-            if (GUILayout.Button("Server"))
+            }
+            else if (GUILayout.Button("Server"))
+            {
+                NetworkManager.Singleton.ConnectionApprovalCallback += ApprovalCheck;
                 NetworkManager.Singleton.StartServer();
-            if (GUILayout.Button("Client"))
+            }
+
+            else if (GUILayout.Button("Client"))
                 NetworkManager.Singleton.StartClient();
+        }
+
+        private void ApprovalCheck(byte[] connectionData, ulong clientId,
+            NetworkManager.ConnectionApprovedDelegate callback)
+        {
+            bool approve = true, createPlayerObject = true;
+            
+            Debug.Log("check connection approval");
+
+
+            uint? playerPrefabHash = null;
+
+            Vector3? positionToSpawnAt = Vector3.zero;
+            Quaternion rotation = Quaternion.identity;
+
+            callback(createPlayerObject, playerPrefabHash, approve, positionToSpawnAt, rotation);
         }
     }
 }
