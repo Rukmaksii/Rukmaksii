03a681c0369117167375dd1ccba26daaaa470b6e
m1dugh <romain103paris@gmail.com>
2022-01-18 23:22:30 +0100

    adds test class subclass of BasePlayer

---
+++ Assets/Scripts/PlayerControllers/TestClassPlayer.cs
@@ -0,0 +1,10 @@
+﻿namespace PlayerControllers
+{
+    public class TestClassPlayer : BasePlayer
+    {
+        public override string ClassName { get; } = "test class";
+        protected override int maxHealth { get; } = 100;
+
+        protected override float movementSpeed { get; } = 30f;
+    }
+}
