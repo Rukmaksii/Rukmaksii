394244274f5c2aacf2ba1d945d2ddde0778af5ac
m1dugh <romain103paris@gmail.com>
2022-01-19 00:02:16 +0100

    adds Scriptable object for class choice

---
+++ Assets/Scripts/GameManagers/ConnectionScriptableObject.cs
@@ -0,0 +1,12 @@
+﻿using model;
+using UnityEngine;
+
+namespace GameManagers
+{
+    
+    [CreateAssetMenu(fileName = "ConnectionData", menuName = "Rukmaksii/ConnectionScriptableObject")]
+    public class ConnectionScriptableObject : ScriptableObject
+    {
+        public ConnectionData Data = null;
+    }
+}
---
+++ Assets/Scripts/model/ConnectionData.cs
@@ -0,0 +1,18 @@
+﻿namespace model
+{
+
+
+    public class ConnectionData
+    {
+        /**
+         * <value>the name of the class chosen by the player</value>
+         */
+        public string ClassName = "";
+
+        /**
+         * <value>the type of session the <see cref="Unity.Netcode.NetworkManager"/> should start</value>
+         */
+        public string ConnectionType = "host";
+
+    }
+}
