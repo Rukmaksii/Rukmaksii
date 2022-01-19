f76f7b1077f1ae59fc6fb36db34c21c574400a0a
m1dugh <romain103paris@gmail.com>
2022-01-19 11:40:10 +0100

    adds filled scriptable object to connection scene

--- Assets/Scripts/StartMenu/StartMenuHandler.cs
+++ Assets/Scripts/StartMenu/StartMenuHandler.cs
@@ -1,3 +1,6 @@
+using System.Diagnostics;
+using GameManagers;
+using model;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,26 +8,51 @@ namespace StartMenu
 {
     public class StartMenuHandler : MonoBehaviour
     {
-
-
         [SerializeField] private Dropdown connectionType;
         [SerializeField] private Dropdown chosenClass;
+
+        [SerializeField] private ConnectionScriptableObject connectionData;
 
         // Start is called before the first frame update
         void Start()
         {
-        
         }
 
         // Update is called once per frame
         void Update()
         {
-        
         }
 
         public void OnPlay()
         {
-            Debug.Log($"class: {chosenClass.itemText.text}, connection type: {connectionType.itemText.text}");
+            connectionData.Data = new ConnectionData();
+
+            // TODO : set a correct implementation for class choice
+            switch (chosenClass.value)
+            {
+                case 0:
+                    connectionData.Data.ClassName = "test class";
+                    break;
+                default:
+                    connectionData.Data.ClassName = "test class";
+                    break;
+            }
+
+            // TODO : safen connection type choice
+            switch (connectionType.value)
+            {
+                case 0:
+                    connectionData.Data.ConnectionType = "host";
+                    break;
+                case 1:
+                    connectionData.Data.ConnectionType = "client";
+                    break;
+                default:
+                    connectionData.Data.ConnectionType = "server";
+                    break;
+            }
+
+
             UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");
         }
     }
