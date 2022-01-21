9c7b48c9bcf7847a86c7e641cde31c3ea39a1991
m1dugh <romain103paris@gmail.com>
2022-01-21 21:56:14 +0100

    creates test rifle for multi bullet testing

---
+++ Assets/Scripts/Weapons/TestRifleGun.cs
@@ -0,0 +1,16 @@
+﻿using model;
+
+namespace Weapons
+{
+    public class TestRifleGun : BaseWeapon
+    {
+        public override WeaponType Type { get; } = WeaponType.Heavy;
+        public override float Range { get; } = 100f;
+        public override int Damage { get; } = 3;
+        public override float Cooldown { get; } = 1f;
+        public override int MaxAmmo { get; } = 50;
+        public override float ReloadTime { get; } = 5f;
+        public override int BulletsInRow { get; } = 5;
+        public override float BulletsInRowSpacing { get; } = 0.1f;
+    }
+}
