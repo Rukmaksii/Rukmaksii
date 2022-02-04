2867a2c222f266a5aaad0345a93b9faf849e50b2
Emmanuel <emmanuel.vouillon@gmail.com>
2022-02-04 20:16:47 +0100

    implements functions behind main menu buttons

--- Assets/Scripts/Menus/StartMenuHandler.cs
+++ Assets/Scripts/Menus/StartMenuHandler.cs
@@ -3,11 +3,15 @@ using GameManagers;
 using model;
 using UnityEngine;
 using UnityEngine.UI;
+using Debug = UnityEngine.Debug;
 
 namespace Menus
 {
     public class StartMenuHandler : MonoBehaviour
     {
+        [SerializeField] private GameObject mainMenu;
+        [SerializeField] private GameObject selectMenu;
+        
         [SerializeField] private Dropdown connectionType;
         [SerializeField] private Dropdown chosenClass;
 
@@ -16,6 +20,7 @@ namespace Menus
         // Start is called before the first frame update
         void Start()
         {
+            selectMenu.SetActive(false);
         }
 
         // Update is called once per frame
@@ -60,26 +65,47 @@ namespace Menus
 
         public void OnPlay()
         {
+            switch (chosenClass.value)
+            {
+                case 0:
+                    connectionData.Data.ClassName = "test class";
+                    break;
+                default:
+                    connectionData.Data.ClassName = "test class";
+                    break;
+            }
             UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");
         }
         
         public void OnSingleplayer()
         {
+            connectionData.Data = new ConnectionData();
+            
             connectionData.Data.ConnectionType = "host";
+            
+            mainMenu.SetActive(false);
+            selectMenu.SetActive(true);
         }
         
         public void OnMultiplayer()
         {
+            connectionData.Data = new ConnectionData();
+            
             connectionData.Data.ConnectionType = "client";
+            
+            mainMenu.SetActive(false);
+            selectMenu.SetActive(true);
         }
         
         public void OnOptions()
         {
             // TODO
+            Debug.Log("Entering options menu");
         }
         
         public void OnQuit()
         {
+            Debug.Log("Quitting the game");
             Application.Quit();
         }
     }
