b43f7c92ec0e5de4f2c3901bf32aded580b71093
Alban Naulin <alban.naulin@epita.fr>
2022-03-07 15:44:47 +0100

    fix the monsters' death

--- Assets/Scripts/Monster/MonsterControler.cs
+++ Assets/Scripts/Monster/MonsterControler.cs
@@ -42,7 +42,7 @@ namespace MonstersControler
             {
                 if (monster != null)
                 {
-                    Destroy(monster.gameObject);
+                    monster.gameObject.SetActive(false);
                 }
                 else
                 {
