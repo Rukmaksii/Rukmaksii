8bbc34cd15eb53193519aa194a1e1284f09a8274
m1dugh <romain103paris@gmail.com>
2022-03-09 17:09:44 +0100

    creates base minion class

---
+++ Assets/Scripts/Minions/BaseMinion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using model;
+using Unity.Netcode;
+using Unity.Netcode.Components;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Minions
+{
+    [RequireComponent(typeof(NetworkTransform))]
+    [RequireComponent(typeof(NetworkRigidbody))]
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class BaseMinion : NetworkBehaviour, IMinion
+    {
+        
+        [SerializeField]
+        private float lookRadius = 10f;
+        
+        public void Aim()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void MoveTo(Vector3 position)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Fire()
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+        }
+    }
+}
