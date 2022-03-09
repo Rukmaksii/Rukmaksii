6dc76377a294437c9b631d65bf0e53191eea24b6
m1dugh <romain103paris@gmail.com>
2022-03-09 21:47:58 +0100

    adds IKillable interface

---
+++ Assets/Scripts/model/IKillable.cs
@@ -0,0 +1,17 @@
+﻿namespace model
+{
+    public interface IKillable
+    {
+        /**
+         * <summary>inflicts <see cref="damage"/> to the <see cref="IKillable"/></summary>
+         * <param name="damage">the damages to be inflicted</param>
+         * <returns>true if the <see cref="IKillable"/> is still alive, false otherwise</returns>
+         */
+        public bool TakeDamage(int damage);
+
+        /**
+         * <summary>called when an <see cref="IKillable"/> is killed</summary>
+         */
+        public void OnKill();
+    }
+}
