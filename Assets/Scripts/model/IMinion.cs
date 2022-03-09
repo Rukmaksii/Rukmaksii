2f67f40b1392bed9f28fdd5d43b05f8362c99cec
m1dugh <romain103paris@gmail.com>
2022-03-09 17:02:34 +0100

    adds IMinion interface

---
+++ Assets/Scripts/model/IMinion.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace model
+{
+    public interface IMinion
+    {
+     
+        /**
+         * <summary>the strategy used for the minion</summary>
+         */
+        public enum Strategy
+        {
+            /**
+             * <value>defend a spot on the map</value>
+             */
+            DEFEND,
+            /**
+             * <value>Protects a player</value>
+             */
+            PROTECT,
+            
+            /**
+             * <value>attack enemy players and follow them</value>
+             */
+            ATTACK
+        }
+        
+        /**
+         * <summary>aims at a target</summary>
+         */
+        public void Aim();
+
+        /**
+         * <summary>moves to a given position</summary>
+         */
+        public void MoveTo(Vector3 position);
+        
+        /**
+         * <summary>fires at a target</summary>
+         */
+        public void Fire();
+    }
+}
