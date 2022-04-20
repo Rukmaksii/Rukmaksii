d571c11394db19fee7b9639b3bcdd28a22c2fece
m1dugh <romain103paris@gmail.com>
2022-04-20 20:32:21 +0200

    Inventory: moves item container to name space items

--- Assets/Scripts/Items/ItemContainer.cs
+++ Assets/Scripts/Items/ItemContainer.cs
@@ -1,45 +1,47 @@
 using System.Collections.Generic;
-using Items;
 using model;
 
-public class ItemContainer<TBaseItem> where TBaseItem : BaseItem
+namespace Items
 {
-    public readonly int MaxCount;
-    private Stack<TBaseItem> items = new Stack<TBaseItem>();
-    public int Count => items.Count;
-    private ItemCategory category;
-    public ItemCategory Category => category;
+    public class ItemContainer<TBaseItem> where TBaseItem : BaseItem
+    {
+        public readonly int MaxCount;
+        private Stack<TBaseItem> items = new Stack<TBaseItem>();
+        public int Count => items.Count;
+        private ItemCategory category;
+        public ItemCategory Category => category;
 
-    private readonly Inventory inventory;
+        private readonly Inventory inventory;
 
     
 
-    public ItemContainer(int maxCount, Inventory inventory)
-    {
-        this.MaxCount = maxCount;
-        this.inventory = inventory;
-    }
-
-    public bool Push(TBaseItem item, bool present = false)
-    {
-        if (items.Count < MaxCount)
+        public ItemContainer(int maxCount, Inventory inventory)
         {
-            items.Push(item);
-            if (!present)
-                inventory.AddItem(item);
-            return true;
+            this.MaxCount = maxCount;
+            this.inventory = inventory;
         }
 
-        return false;
-    }
-
-    public TBaseItem Pop()
-    {
-        if (items.Count > 0)
+        public bool Push(TBaseItem item, bool present = false)
         {
-            return items.Pop();
+            if (items.Count < MaxCount)
+            {
+                items.Push(item);
+                if (!present)
+                    inventory.AddItem(item);
+                return true;
+            }
+
+            return false;
         }
 
-        return null;
+        public TBaseItem Pop()
+        {
+            if (items.Count > 0)
+            {
+                return items.Pop();
+            }
+
+            return null;
+        }
     }
 }
