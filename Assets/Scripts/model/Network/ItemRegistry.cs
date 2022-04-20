9736fea4e057a99a8ba4cbb3b0658f437ee06846
m1dugh <romain103paris@gmail.com>
2022-04-21 01:36:52 +0200

    Inventory: creates item registry

---
+++ Assets/Scripts/model/Network/ItemRegistry.cs
@@ -0,0 +1,27 @@
+﻿using Unity.Netcode;
+
+namespace model.Network
+{
+    public class ItemRegistry : NetworkVariableBase
+    {
+        public override void WriteDelta(FastBufferWriter writer)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void WriteField(FastBufferWriter writer)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void ReadField(FastBufferReader reader)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void ReadDelta(FastBufferReader reader, bool keepDirtyDelta)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
