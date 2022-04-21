f1ac8ad9ee5a7872e1c249eecc87eb5adb6d3d54
Emmanuel Vouillon <emmanuel.vouillon@gmail.com>
2022-04-21 19:04:44 +0200

    fixes objectives on the minimap

--- Assets/Scripts/HUD/HUDMiniMap.cs
+++ Assets/Scripts/HUD/HUDMiniMap.cs
@@ -59,26 +59,26 @@ namespace HUD
         private void UpdateObjectives()
         {
             ObjectiveController obj1 = GameObject.Find("Objective1").GetComponentInChildren<ObjectiveController>();
-            ObjectiveController obj2 = GameObject.Find("Objective2").GetComponentInChildren<ObjectiveController>();
-            ObjectiveController obj3 = GameObject.Find("Objective3").GetComponentInChildren<ObjectiveController>();
+            ObjectiveController obj3 = GameObject.Find("Objective2").GetComponentInChildren<ObjectiveController>();
+            ObjectiveController obj2 = GameObject.Find("Objective3").GetComponentInChildren<ObjectiveController>();
 
-            if (obj1.CapturingTeam == 0)
+            if (obj1.CurrentState == ObjectiveController.State.Captured && obj1.CapturingTeam == 0)
                 objOverlay1.color = Color.blue;
-            else if (obj1.CapturingTeam == 1)
+            else if (obj1.CurrentState == ObjectiveController.State.Captured && obj1.CapturingTeam == 1)
                 objOverlay1.color = Color.red;
             else
                 objOverlay1.color = Color.white;
             
-            if (obj2.CapturingTeam == 0)
+            if (obj2.CurrentState == ObjectiveController.State.Captured && obj2.CapturingTeam == 0)
                 objOverlay2.color = Color.blue;
-            else if (obj1.CapturingTeam == 1)
+            else if (obj2.CurrentState == ObjectiveController.State.Captured && obj1.CapturingTeam == 1)
                 objOverlay2.color = Color.red;
             else
                 objOverlay2.color = Color.white;
             
-            if (obj3.CapturingTeam == 0)
+            if (obj3.CurrentState == ObjectiveController.State.Captured && obj3.CapturingTeam == 0)
                 objOverlay3.color = Color.blue;
-            else if (obj1.CapturingTeam == 1)
+            else if (obj3.CurrentState == ObjectiveController.State.Captured && obj1.CapturingTeam == 1)
                 objOverlay3.color = Color.red;
             else
                 objOverlay3.color = Color.white;
