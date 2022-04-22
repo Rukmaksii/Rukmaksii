8ecf325821a8cc9436799a7e3063f7ef2c9869f5
Alban Naulin <alban.naulin@epita.fr>
2022-04-22 11:15:28 +0200

    shield controller

---
+++ Assets/Scripts/Map/ShieldController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class ShieldController : NetworkBehaviour
+{
+    private NetworkVariable<bool> activated = new NetworkVariable<bool>(true);
+
+    public NetworkVariable<bool> Activated
+    {
+        get => activated;
+        set => activated = value;
+    }
+
+    private int teamId;
+
+    public int TeamId
+    {
+        get => teamId;
+        set => teamId = value;
+    }
+
+    private new MeshCollider collider;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = gameObject.GetComponent<MeshCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        collider.enabled = activated.Value;
+    }
+
+    public void ChangeActivation(bool activated)
+    {
+        collider.enabled = activated;
+    }
+    
+    [ServerRpc]
+    public void UpdateTeamServerRpc(int teamId)
+    {
+        this.teamId = teamId;
+    }
+}
