3da872801471c89f8ffb0a0bc420cbc115189efc
m1dugh <romain103paris@gmail.com>
2022-04-25 14:16:27 +0200

    Ability: adds ability tree

---
+++ Assets/Scripts/Abilities/AbilityTree.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using PlayerControllers;
+
+namespace Abilities
+{
+    public class AbilityTree
+    {
+        private readonly BasePlayer player;
+        public BaseAbility CurrentAbility { get; private set; }
+        public readonly List<BaseAbility> Abilities = new List<BaseAbility>();
+
+        public AbilityTree(BasePlayer player, BaseAbility currentAbility)
+        {
+            this.player = player;
+            CurrentAbility = currentAbility;
+        }
+
+        public void ChooseAbility(Type t)
+        {
+            if (CurrentAbility == null)
+                return;
+        }
+
+        private void InstantiateAbility(Type t)
+        {
+            CurrentAbility = (BaseAbility) t.GetConstructor(new Type[] {typeof(BasePlayer)})!.Invoke(new object[] {player});
+            CurrentAbility.Apply();
+        }
+    }
+}
