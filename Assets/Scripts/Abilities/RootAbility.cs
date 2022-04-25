d58625e84fb2dedc8419bc33090f53920df3c236
m1dugh <romain103paris@gmail.com>
2022-04-25 15:39:29 +0200

    Ability: adds root ability abstract class

---
+++ Assets/Scripts/Abilities/RootAbility.cs
@@ -0,0 +1,15 @@
+﻿using PlayerControllers;
+
+namespace Abilities
+{
+    public abstract class RootAbility : BaseAbility
+    {
+        protected RootAbility(BasePlayer player) : base(player)
+        {
+        }
+
+        public override void Apply()
+        {
+        }
+    }
+}
