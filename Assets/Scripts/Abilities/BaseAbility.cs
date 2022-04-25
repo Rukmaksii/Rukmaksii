e545c39f6349376eb4189a51583bf94d641e003f
m1dugh <romain103paris@gmail.com>
2022-04-25 13:34:13 +0200

    Ability: adds ability info dict

--- Assets/Scripts/Abilities/BaseAbility.cs
+++ Assets/Scripts/Abilities/BaseAbility.cs
@@ -5,12 +5,29 @@ using PlayerControllers;
 
 namespace Abilities
 {
+    public struct AbilityInfo
+    {
+        public string Name;
+        public List<Type> ChildrenAbilities;
+    }
+
     public abstract class BaseAbility : IAbility
     {
-        protected BasePlayer Player;
-        public abstract string Name { get; }
+        public readonly static Dictionary<Type, AbilityInfo> AbilityInfos = new Dictionary<Type, AbilityInfo>()
+        {
+            {
+                typeof(Jetpack), new AbilityInfo()
+                {
+                    Name = "Jetpack Ability",
+                    ChildrenAbilities = new List<Type>
+                    {
+                    }
+                }
+            }
+        };
 
-        public abstract List<Type> ChildrenAbilities { get; }
+        protected BasePlayer Player;
+        public AbilityInfo Info => AbilityInfos[GetType()];
 
 
         public BaseAbility(BasePlayer player)
--- Assets/Scripts/Abilities/JetpackAbility.cs
+++ Assets/Scripts/Abilities/JetpackAbility.cs
@@ -7,8 +7,6 @@ namespace Abilities
 {
     public class JetpackAbility : BaseAbility
     {
-        public override string Name { get; } = "jetpack";
-        public override List<Type> ChildrenAbilities { get; } = new List<Type>();
         private float multiplier = 1.1f;
 
         public override void Apply()
