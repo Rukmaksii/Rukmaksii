6f4eb85ac8f1ec97fb90b3cf90bd4ca92fab9dd9
m1dugh <romain103paris@gmail.com>
2022-05-09 22:49:27 +0200

    Network: creates NetworkPlayersRegistry empty network variable base

---
+++ Assets/Scripts/model/NetworkPlayersRegistry.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+
+namespace model
+{
+    public class NetworkPlayersRegistry<TNSerializable> : NetworkVariableBase, IDictionary<ulong, TNSerializable>
+        where TNSerializable : INetworkSerializable
+    {
+        public override void WriteDelta(FastBufferWriter writer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WriteField(FastBufferWriter writer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ReadField(FastBufferReader reader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ReadDelta(FastBufferReader reader, bool keepDirtyDelta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<KeyValuePair<ulong, TNSerializable>> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(KeyValuePair<ulong, TNSerializable> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(KeyValuePair<ulong, TNSerializable> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(KeyValuePair<ulong, TNSerializable>[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(KeyValuePair<ulong, TNSerializable> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count { get; }
+        public bool IsReadOnly { get; }
+
+        public void Add(ulong key, TNSerializable value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ContainsKey(ulong key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(ulong key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryGetValue(ulong key, out TNSerializable value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TNSerializable this[ulong key]
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+
+        public ICollection<ulong> Keys { get; }
+        public ICollection<TNSerializable> Values { get; }
+    }
+}
