da342bc0588ec39fc58b4895ff6eaa9ba6394e07
m1dugh <romain103paris@gmail.com>
2022-05-09 17:41:40 +0200

    Lobby: creates lobby manager

---
+++ Assets/Scripts/LobbyScene/LobbyManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using GameManagers;
+using UnityEngine;
+
+public class LobbyManager : MonoBehaviour
+{
+
+    [SerializeField] private ConnectionManagerScript connectionManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
