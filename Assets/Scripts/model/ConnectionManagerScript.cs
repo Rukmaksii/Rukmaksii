5424ce69aca2d9c663421454f32c8a55571cd5f4
m1dugh <romain103paris@gmail.com>
2022-05-09 19:19:15 +0200

    Network: refactors debug logic in connection manager

--- Assets/Scripts/model/ConnectionManagerScript.cs
+++ Assets/Scripts/model/ConnectionManagerScript.cs
@@ -1,4 +1,6 @@
+using System;
 using model;
+using PlayerControllers;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UNET;
 using UnityEngine;
@@ -12,33 +14,34 @@ namespace GameManagers
 
         void Start()
         {
-#if UNET
+#if DEBUG
             NetworkManager.Singleton.NetworkConfig.NetworkTransport = GetComponent<UNetTransport>();
 #endif
-
-
-            // kept for local tests
-            // TODO : remove this line and OnGui
-            if (!connectionData.Data.IsReady)
-                return;
-
-
-            switch (connectionData.Data.ConnectionType)
-            {
-                case "host":
-                    NetworkManager.Singleton.StartHost();
-                    break;
-                case "client":
-                    NetworkManager.Singleton.StartClient();
-                    break;
-                case "server":
-                    NetworkManager.Singleton.StartServer();
-                    break;
-            }
-
-            //GameController.Singleton.PlayerUIInstance.SetActive(true);
         }
 
+        private void Update()
+        {
+            if (!NetworkManager.Singleton.IsServer && !NetworkManager.Singleton.IsClient)
+            {
+                if (connectionData.Data.IsReady)
+                {
+                    switch (connectionData.Data.ConnectionType)
+                    {
+                        case "host":
+                            NetworkManager.Singleton.StartHost();
+                            break;
+                        case "client":
+                            NetworkManager.Singleton.StartClient();
+                            break;
+                        case "server":
+                            NetworkManager.Singleton.StartServer();
+                            break;
+                    }
+                }
+            }
+        }
+
+#if DEBUG
         private void OnGUI()
         {
             GUILayout.BeginArea(new Rect(10, 10, 300, 300));
@@ -54,22 +57,28 @@ namespace GameManagers
         {
             if (GUILayout.Button("Host"))
             {
-                NetworkManager.Singleton.StartHost();
+                connectionData.Data.ConnectionType = "host";
             }
             else if (GUILayout.Button("Server"))
             {
-                NetworkManager.Singleton.StartServer();
+                connectionData.Data.ConnectionType = "server";
             }
 
             else if (GUILayout.Button("Client"))
             {
-                NetworkManager.Singleton.StartClient();
+                connectionData.Data.ConnectionType = "client";
             }
             else
             {
                 return;
             }
+
+            connectionData.Data.ClassName = "test class";
+            connectionData.Data.TeamId = 0;
+
             GameController.Singleton.PlayerUIInstance.SetActive(true);
         }
     }
+
+#endif
 }
