116795732b013862b4c14f3f75e196251995f3f0
EmmanuelVln <45790349+EmmanuelVln@users.noreply.github.com>
2022-05-30 15:29:48 +0200

    Class: adds Tank class

---
+++ Assets/Scripts/GameScene/PlayerControllers/TankClassPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using GameScene.Abilities.model;
+
+namespace GameScene.PlayerControllers
+{
+    public class TankClassPlayer : BasePlayer.BasePlayer
+    {
+        public override string ClassName { get; } = "Tank";
+        public override int MaxHealth { get; protected set; } = 125;
+        public override RootAbility RootAbility { get; } = new TestClassRoot();
+        protected override float movementSpeed { get; } = 5f;
+
+        protected override float runningSpeedMultiplier { get; } = 1.75f;
+
+        protected override float jumpForce { get; } = 3;
+        
+        public override float DefaultFuelDuration { get; } = 7.5f;
+
+        protected override float dashDuration { get; set; } = .2f;
+
+        protected override float dashForce { get; set; } = 22.5f;
+
+        protected override float gravityMultiplier { get; } = 0.6f;
+
+        public override Type WeaponInterface { get; } = typeof(IWeapon);
+
+        public interface IWeapon : model.IWeapon
+        {
+        }
+    }
+}
