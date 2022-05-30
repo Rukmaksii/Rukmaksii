843d880d4b851f801ac4a7026b02c0af7355ee5f
m1dugh <romain103paris@gmail.com>
2022-05-30 14:29:18 +0200

    Players: adds scout class player

---
+++ Assets/Scripts/GameScene/PlayerControllers/ScoutClassPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using GameScene.Abilities.model;
+
+namespace GameScene.PlayerControllers
+{
+    public class ScoutClassPlayer : BasePlayer.BasePlayer
+    {
+        public override string ClassName { get; } = "Scout";
+        public override int MaxHealth { get; protected set; } = 75;
+        public override RootAbility RootAbility { get; } = new TestClassRoot();
+        protected override float movementSpeed { get; } = 7.5f;
+
+        protected override float runningSpeedMultiplier { get; } = 2.25f;
+
+        protected override float jumpForce { get; } = 7.5f;
+
+        public override float DefaultFuelDuration { get; } = 12.5f;
+
+        protected override float dashDuration { get; set; } = .2f;
+
+        protected override float dashForce { get; set; } = 45f;
+
+        protected override float gravityMultiplier { get; } = 1.3f;
+
+        public override Type WeaponInterface { get; } = typeof(IWeapon);
+
+        public interface IWeapon : model.IWeapon
+        {
+        }
+    }
+}
--- Assets/Scripts/GameScene/PlayerControllers/SoldierClassPlayer.cs
+++ Assets/Scripts/GameScene/PlayerControllers/SoldierClassPlayer.cs
@@ -5,7 +5,7 @@ namespace GameScene.PlayerControllers
 {
     public class SoldierClassPlayer : BasePlayer.BasePlayer
     {
-        public override string ClassName { get; } = "test class";
+        public override string ClassName { get; } = "Soldier";
         public override int MaxHealth { get; protected set; } = 100;
         public override RootAbility RootAbility { get; } = new TestClassRoot();
         protected override float movementSpeed { get; } = 7.5f;
@@ -22,7 +22,7 @@ namespace GameScene.PlayerControllers
 
         protected override float gravityMultiplier { get; } = 1f;
 
-        public override Type WeaponInterface { get; } = typeof(SoldierClassPlayer.IWeapon);
+        public override Type WeaponInterface { get; } = typeof(IWeapon);
 
         public interface IWeapon : model.IWeapon
         {
