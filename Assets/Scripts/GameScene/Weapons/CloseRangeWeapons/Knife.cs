4ded7bda32715568fe2a514c1dfa4f998e106220
m1dugh <romain103paris@gmail.com>
2022-05-30 16:01:03 +0200

    Weapons: implements knife in lightweapons

---
+++ Assets/Scripts/GameScene/Weapons/CloseRangeWeapons/Knife.cs
@@ -0,0 +1,18 @@
+﻿using GameScene.model;
+using GameScene.PlayerControllers;
+
+namespace GameScene.Weapons.CloseRangeWeapons
+{
+    public class Knife : BaseWeapon, SoldierClassPlayer.IWeapon, ScoutClassPlayer.IWeapon, TankClassPlayer.IWeapon
+    {
+        public override WeaponType Type { get; } = WeaponType.CloseRange;
+        public override float Range { get; } = 0.5f;
+        public override int Damage { get; } = 35;
+        public override string Name { get; } = "Knife";
+        public override float Cooldown { get; } = 0;
+        public override int MaxAmmo { get; } = 1;
+        public override float ReloadTime { get; } = 0.3f;
+        public override int BulletsInRow { get; } = 0;
+        public override float BulletsInRowSpacing { get; } = 0;
+    }
+}
