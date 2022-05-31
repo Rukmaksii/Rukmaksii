e5e8d57dc809fd74d3302c50e6ad778590ea4081
m1dugh <romain103paris@gmail.com>
2022-05-31 18:56:27 +0200

    UI: deactivates wheel when not called

--- Assets/Scripts/GameScene/HUD/MinionWheelController.cs
+++ Assets/Scripts/GameScene/HUD/MinionWheelController.cs
@@ -1,9 +1,13 @@
+using System.Collections.Generic;
 using GameScene.model;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 namespace GameScene.HUD
 {
+    [RequireComponent(typeof(GraphicRaycaster))]
     public class MinionWheelController : MonoBehaviour
     {
         private bool active = false;
@@ -12,19 +16,19 @@ namespace GameScene.HUD
 
         private Vector2 basePosition = Vector2.zero;
 
+        private GraphicRaycaster raycaster;
+
         public bool IsActive
         {
             get => active;
             set
             {
+                gameObject.SetActive(value);
+                GetComponent<Canvas>().enabled = value;
+                active = value;
                 if (value)
                 {
-                    gameObject.SetActive(false);
-                    // Activate(Mouse.current.position.ReadValue());
-                }
-                else
-                {
-                    gameObject.SetActive(false);
+                    Activate(Mouse.current.position.ReadValue());
                 }
             }
         }
@@ -32,32 +36,50 @@ namespace GameScene.HUD
         // Start is called before the first frame update
         void Start()
         {
-            foreach (var child in gameObject.GetComponentsInChildren<RectTransform>())
-            {
-                if (child.parent != transform)
-                    continue;
-            }
+            raycaster = GetComponent<GraphicRaycaster>();
         }
 
         // Update is called once per frame
         // ReSharper disable Unity.PerformanceAnalysis
         void Update()
         {
+            PointerEventData ev = new PointerEventData(EventSystem.current);
+            ev.position = Mouse.current.position.ReadValue();
+            List<RaycastResult> results = new List<RaycastResult>();
+            raycaster.Raycast(ev, results);
+
+            bool foundElement = false;
+
+            foreach (var child in GetComponentsInChildren<RectTransform>())
+            {
+                if (child.parent != transform)
+                    continue;
+
+
+                RectTransform foundChild = null;
+                for (int i = 0; !foundElement && i < results.Count && foundChild == null; i++)
+                {
+                    if (results[i].gameObject.GetComponent<RectTransform>().parent == child.transform ||
+                        results[i].gameObject == child.gameObject)
+                    {
+                        foundChild = child;
+                    }
+                }
+
+                if (foundChild != null)
+                {
+                    foundChild.localScale = new Vector3(1.3f, 1.3f, 0);
+                    foundElement = true;
+                }
+                else
+                {
+                    child.localScale = new Vector3(1, 1, 1);
+                }
+            }
         }
 
         private void Activate(Vector2 position)
         {
-        }
-
-
-        public void OnPointerEnter(PointerEventData eventData)
-        {
-            Debug.Log($"entering {eventData.hovered.Count}");
-        }
-
-        public void OnPointerExit(PointerEventData eventData)
-        {
-            Debug.Log($"exiting {eventData.hovered.Count}");
         }
     }
 }
