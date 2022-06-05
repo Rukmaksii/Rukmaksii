4dd1b7aa21ad5790c2ea28e81128cfa6d1a25e53
EmmanuelVln <emmanuel.vouillon@epita.fr>
2022-06-05 16:53:02 +0200

    Weapons: tweaks a few stats

--- Assets/Scripts/GameScene/Weapons/HeavyWeapons/UMP.cs
+++ Assets/Scripts/GameScene/Weapons/HeavyWeapons/UMP.cs
@@ -7,10 +7,10 @@ namespace GameScene.Weapons.HeavyWeapons
     {
         public override WeaponType Type { get; } = WeaponType.Heavy;
         public override float Range { get; } = 50;
-        public override int Damage { get; } = 3;
+        public override int Damage { get; } = 4;
         public override float Cooldown { get; } = 0.1f;
         public override int MaxAmmo { get; } = 25;
-        public override float ReloadTime { get; } = 1.5f;
+        public override float ReloadTime { get; } = 1f;
         public override int BulletsInRow { get; } = 1;
         public override float BulletsInRowSpacing { get; } = 0.1f;
         public override int Price { get; } = 600;
