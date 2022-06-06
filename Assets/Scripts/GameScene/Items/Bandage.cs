33c0d1981ec9a8b0015bfc1dff631f8c4c449938
EmmanuelVln <emmanuel.vouillon@epita.fr>
2022-06-06 14:45:11 +0200

    Items: starts implementing bandages

---
+++ Assets/Scripts/GameScene/Items/Bandage.cs
@@ -0,0 +1,32 @@
+﻿using GameScene.Map;
+using GameScene.model;
+using GameScene.PlayerControllers.BasePlayer;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace GameScene.Items
+{
+    public class Bandage : BaseItem
+    {
+        public override float Duration { get; } = 2f;
+        public override int Price { get; } = 30;
+
+        protected override void Setup()
+        {
+            Player.EffectiveMovementSpeed = 0;
+            Player.Jetpack.JetpackForce = 0;
+        }
+
+
+        
+        protected override void OnConsume()
+        {
+            
+        }
+
+        protected override void TearDown()
+        {
+
+        }
+    }
+}
