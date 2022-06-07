766eb1bcaa79fe59ea0ed7704cab4e1730a8dab9
EmmanuelVln <45790349+EmmanuelVln@users.noreply.github.com>
2022-06-07 10:53:56 +0200

    Fixes the objectives' cap time and the bases' health

--- Assets/Scripts/GameScene/Map/BaseController.cs
+++ Assets/Scripts/GameScene/Map/BaseController.cs
@@ -6,7 +6,7 @@ namespace GameScene.Map
 {
     public class BaseController : MonoBehaviour, IKillable
     {
-        private int _currentHealth = 3;
+        private int _currentHealth = 30000;
 
         public int CurrentHealth => _currentHealth;
 
